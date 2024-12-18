10748d54300f11d3f786e3b45ce281653221a0e6
NikaGogelia <gogelianika1@gmail.com>
2024-12-18 19:24:07 +0400

    Add project files.

---
+++ ClassesAndObjectsTask/Employee.cs
@@ -0,0 +1,40 @@
+﻿namespace ClassesAndObjectsTask
+{
+    public class Employee
+    {
+        private string surname = string.Empty;
+        private int age = 0;
+
+        public Employee()
+        {
+        }
+
+        public Employee(string surname, int age)
+        {
+            this.surname = surname;
+            this.age = age;
+        }
+
+        private string ConvertAgeIntoString(int age = 0)
+        {
+            if (age < 0)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return age.ToString();
+            }
+        }
+
+        public void ChangeSurname(string newSurname)
+        {
+            this.surname = newSurname;
+        }
+
+        public string GetEmployee()
+        {
+            return $"Surname: {this.surname}, Age: {this.age}";
+        }
+    }
+}
